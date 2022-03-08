5fd75ea4137fc8a562a02c7804ffd1454cfc2e7b
virahul77 <vishwakarma.rahul77@gmail.com>
2022-03-08 21:11:19 +0530

    tut 63 completed. Restructured and Added Addnote Functionality

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,18 +1,21 @@
-import React , { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
-import Noteitem from './Noteitem'
+import React, { useContext } from "react";
+
+import AddNote from "./AddNote";
+import Noteitem from "./Noteitem";
 
 const Notes = () => {
-    const contex = useContext(noteContext)
-  const {notes,setNotes} = contex
+  
   return (
-    <div className="row my-3">
-      <h2>Your Notes</h2>
-      {notes.map((note)=>{
-        return <Noteitem key={note._id} note = {note} />
-      })}
+    <>
+      <AddNote />
+      <div className="row my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
       </div>
-  )
-}
+    </>
+  );
+};
 
-export default Notes
+export default Notes;
